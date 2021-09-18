19b3b34fbd1d443ad3bfb3c66ec83ebe24d322f7
toofantah <toofan.tah@gmail.com>
2021-09-19 00:20:16 +0300

    UI Builder Added

--- Assets/Scripts/AI Behaviour/BehaviourTreeRunner.cs
+++ Assets/Scripts/AI Behaviour/BehaviourTreeRunner.cs
@@ -13,16 +13,26 @@ public class BehaviourTreeRunner : MonoBehaviour
        var log1 = ScriptableObject.CreateInstance<DebogLogNode>();
         log1.message = "Hello EveryOne! 111";
 
+        var Pause1 = ScriptableObject.CreateInstance<WaitNode>();
+
         var log2 = ScriptableObject.CreateInstance<DebogLogNode>();
         log2.message = "Hello EveryOne! 222";
+
+        var Pause2 = ScriptableObject.CreateInstance<WaitNode>();
 
         var log3 = ScriptableObject.CreateInstance<DebogLogNode>();
         log3.message = "Hello EveryOne! 333";
 
+        var Pause3 = ScriptableObject.CreateInstance<WaitNode>();
+
+
         var sequence = ScriptableObject.CreateInstance<SequencerNode>();
         sequence.children.Add(log1);
+        sequence.children.Add(Pause1);
         sequence.children.Add(log2);
+        sequence.children.Add(Pause2);
         sequence.children.Add(log3);
+        sequence.children.Add(Pause3);
         var loop = ScriptableObject.CreateInstance<RepeatNode>();
         loop.child = sequence;
 
